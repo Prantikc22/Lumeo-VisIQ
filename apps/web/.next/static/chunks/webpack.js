bd2a3088a7a6de3c5317237764a61bbe21444a3b
prantikc22 <prantikchatterjee22@gmail.com>
2025-08-27 15:39:21 +0530

    hosted page updated

--- apps/web/.next/static/chunks/webpack.js
+++ apps/web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a99b13ae341ca900"; }
+/******/ 		__webpack_require__.h = function() { return "a9f3eaa7276b8cdd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
